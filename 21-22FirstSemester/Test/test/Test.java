2579a08c77d6dd8fb6dbde4bfa14fb6c4f930e9c
Matty <wu.hang.gcu@qq.com>
2021-10-28 01:24:56 +0800

    2021/10/28 common backup

--- 21-22FirstSemester/Test/test/Test.java
+++ 21-22FirstSemester/Test/test/Test.java
@@ -1,17 +1,6 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-/**
- * @author Matty's PC
- */
 public class Test {
+    @MyAnnotation(myName = "Wu Hang", myAge = 19)
+    public void method1() {}
 }
-
-class Food {}
-class Fruit extends Food{}
-class Apple extends Fruit {}
-class RedApple extends Apple {}
-class Banana extends Fruit {}
