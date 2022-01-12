dd74b6155e135418ce31df28b0926427ee022a59
Matty <wu.hang.gcu@qq.com>
2022-01-12 15:13:38 +0800

    2022/01/12 周三 common backup

--- 21-22WinterHoliday/src/test/Test.java
+++ 21-22WinterHoliday/src/test/Test.java
@@ -1,12 +1,6 @@
 package test;
 
-import dsaa.ch09.section02.SwapSortTest;
-
-import java.util.Arrays;
-
 public class Test {
     public static void main(String[] args) {
-        "".charAt(0);
-        var a = new SwapSortTest();
     }
 }
