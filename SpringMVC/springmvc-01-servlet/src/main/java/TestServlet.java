0a05a7808950f28697ff35cd1c699ef47cc2f4e0
Matty <wu.hang.gcu@qq.com>
2022-04-07 15:50:24 +0800

    2022/04/07 周四 common backup

---
+++ SpringMVC/springmvc-01-servlet/src/main/java/TestServlet.java
@@ -0,0 +1,35 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Matty
+ **/
+@WebServlet(name = "TestServlet", value = "/TestServlet")
+public class TestServlet extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //1.获取前端参数
+        String method = req.getParameter("method");
+        if("add".equals(method)) {
+            req.getSession().setAttribute("message", "执行了add方法");
+        } else if("delete".equals(method)) {
+            req.getSession().setAttribute("message", "执行了delete方法");
+        } else {
+            req.getSession().setAttribute("message", "方法名为空或方法名不存在");
+        }
+        //2.调用业务层
+        //...
+        //3.视图转发或重定向
+        req.getRequestDispatcher("/WEB-INF/jsp/test.jsp").forward(req, resp);
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
---
+++ SpringMVC/springmvc-01-servlet/web/WEB-INF/jsp/test.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Matty's PC
+  Date: 2022/4/6
+  Time: 22:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    ${message}
+</body>
+</html>
