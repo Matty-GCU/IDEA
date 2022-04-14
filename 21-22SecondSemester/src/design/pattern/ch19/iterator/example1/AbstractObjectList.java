38941515ab50fc624d89451ec31daa895e2981f0
Matty <wu.hang.gcu@qq.com>
2022-04-14 13:07:57 +0800

    2022/04/14 周四 common backup

---
+++ 21-22SecondSemester/src/design/pattern/ch19/iterator/example1/AbstractObjectList.java
@@ -0,0 +1,27 @@
+package design.pattern.ch19.iterator.example1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractObjectList {
+    
+    protected List<Object> objects;
+    
+    public AbstractObjectList(List<Object> objects) {
+        this.objects = objects;
+    }
+    
+    public void addObject(Object object) {
+        this.objects.add(object);
+    }
+    
+    public void removeObject(Object object) {
+        this.objects.remove(object);
+    }
+    
+    public List<Object> getObjects() {
+        return objects;
+    }
+    
+    public abstract AbstractIterator createIterator();
+}
